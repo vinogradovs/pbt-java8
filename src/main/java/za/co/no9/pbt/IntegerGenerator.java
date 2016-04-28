9b31eb1f6f552865895ba8ed7424b754e44cfa93
Graeme Lockley <graemel@no9.co.za>
2016-04-28 18:43:47 +0100

    Improved the random algorithm.

--- src/main/java/za/co/no9/pbt/IntegerGenerator.java
+++ src/main/java/za/co/no9/pbt/IntegerGenerator.java
@@ -1,28 +1,29 @@
 package za.co.no9.pbt;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class IntegerGenerator extends AbstractGenerator<Integer> {
     private final int min;
     private final int max;
 
-    protected IntegerGenerator() {
-        this(Integer.MIN_VALUE, Integer.MAX_VALUE);
-    }
-
-    protected IntegerGenerator(int min, int max) {
+    IntegerGenerator(int min, int max) {
         this.min = min;
         this.max = max;
     }
 
     public static Generator<Integer> from() {
-        return new IntegerGenerator();
+        return new IntegerGenerator(Integer.MIN_VALUE, Integer.MAX_VALUE - 1);
     }
 
     public static Generator<Integer> from(int min, int max) {
+        if (max == Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("Unable to support max value of Integer.MAX_VALUE");
+        }
         return new IntegerGenerator(min, max);
     }
 
     @Override
     public Integer next() {
-        return (int) ((Math.random() * (max - min)) + min + 0.5);
+        return min < max ? ThreadLocalRandom.current().nextInt(min, max + 1) : min;
     }
 }
